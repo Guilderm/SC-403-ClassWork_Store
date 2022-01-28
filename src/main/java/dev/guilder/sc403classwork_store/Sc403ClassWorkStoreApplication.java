cbeacf0f4aa7868f1352620de2838fdf5e44a7f5
Guilder W. Milliner <guilderw@gmail.com>
2022-01-27 21:51:19 -0600

    Inicial

---
+++ src/main/java/dev/guilder/sc403classwork_store/Sc403ClassWorkStoreApplication.java
@@ -0,0 +1,13 @@
+package dev.guilder.sc403classwork_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sc403ClassWorkStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sc403ClassWorkStoreApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dev/guilder/sc403classwork_store/ServletInitializer.java
@@ -0,0 +1,13 @@
+package dev.guilder.sc403classwork_store;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Sc403ClassWorkStoreApplication.class);
+    }
+
+}
